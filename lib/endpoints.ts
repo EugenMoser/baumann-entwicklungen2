ef9aedd93c9b9563034490286d4c134b245ee771
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-08-01 22:24:22 +0100

    create api for section request

--- lib/endpoints.ts
+++ lib/endpoints.ts
@@ -2,3 +2,8 @@ export const getAll =
   (process.env.NEXT_PUBLIC_SITE_URL ??
     process?.env?.NEXT_PUBLIC_VERCEL_URL ??
     'http://localhost:3000') + '/api/products/getAll';
+
+export const getSection =
+  (process.env.NEXT_PUBLIC_SITE_URL ??
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+    'http://localhost:3000') + '/api/products/getSection';
---
+++ src/app/api/products/getSection/route.ts
@@ -0,0 +1,38 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+import { prisma } from '@/db/client';
+
+export const dynamic = 'force-dynamic';
+
+export async function POST(request: NextRequest) {
+  const { category } = await request.json();
+
+  if (!category) {
+    return NextResponse.json(
+      { error: 'Category parameter is missing' },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const productCategory = await prisma.product.findMany({
+      select: {
+        product_id: true,
+        category: true,
+        product_name: true,
+        product_description1: true,
+        product_imagepath_small: true,
+      },
+      where: {
+        category: category,
+      },
+    });
+
+    return NextResponse.json({ data: productCategory });
+  } catch (err) {
+    return NextResponse.error();
+  } finally {
+    await prisma.$disconnect();
+  }
+}
