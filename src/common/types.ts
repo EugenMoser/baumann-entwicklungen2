f504cdf505ce862206572e566e711f675948e169
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-08-01 22:24:34 +0100

    adjust types

--- src/common/types.ts
+++ src/common/types.ts
@@ -2,15 +2,15 @@ export interface Product {
   product_id: number;
   category: string;
   product_name: string;
-  product_description1: string;
-  product_description2: string;
-  product_description3: string;
-  product_description4: string;
+  product_description1?: string;
+  product_description2?: string;
+  product_description3?: string;
+  product_description4?: string;
   product_material: string;
   product_imagepath_small: string;
   product_imagepath_big1: string;
-  product_imagepath_big2: string;
-  product_imagepath_big3: string;
+  product_imagepath_big2?: string;
+  product_imagepath_big3?: string;
   article: Article[];
   colors: Color[];
 }
@@ -21,13 +21,13 @@ export interface Article {
   article_prio: number;
   article_name: string;
   article_description: string;
-  article_description1: string;
-  article_description2: string;
-  article_description3: string;
+  article_description1?: string;
+  article_description2?: string;
+  article_description3?: string;
   vpe1: string;
-  vpe2: string;
-  vpe3: string;
-  vpe4: string;
+  vpe2?: string;
+  vpe3?: string;
+  vpe4?: string;
 }
 
 export interface Color {
@@ -35,4 +35,12 @@ export interface Color {
   color_name: string;
   color_code: string;
   color_suffix: number;
+}
+
+export interface CategoryProps {
+  product_id: number;
+  category: string;
+  product_name: string;
+  product_description1: string;
+  product_imagepath_small: string;
 }
