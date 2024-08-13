1743151bb791edfc2549b460bb54738f1ce8b271
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-08-13 23:20:50 +0100

    change home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,35 @@
-import { MouseEventHandler, Suspense } from 'react';
+'use client';
+import {
+  AppRouterInstance,
+} from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { useRouter } from 'next/navigation';
 
-import axios from 'axios';
+import { strings } from '@/src/constants/strings';
+import { useCreateSection } from '@/src/hooks/useCreateSection';
 
-import { getAll } from '@/lib/endpoints';
+// const getItems = async () => {
+//   const { data } = await axios(getAll);
+//   return data.data;
+// };
 
-import HomePage from './container/home-page/page';
-
-const getItems = async () => {
-  const { data } = await axios(getAll);
-  return data.data;
-};
-
-export default async function Home(): Promise<JSX.Element> {
-  const allProducts: any = await getItems();
-
-  if (!allProducts) return <h2>msgSiteLoadingError</h2>;
+export default function Home(): JSX.Element {
+  const router: AppRouterInstance = useRouter();
 
   return (
     <main>
-      <Suspense fallback={<h1>Loading..</h1>}>
-        <HomePage />
-      </Suspense>
+      <h1 className='text-center text-4xl my-4'>
+        {strings.companyWelcome}
+      </h1>
+
+      <p className='leading-6'>{strings.companyDescription}</p>
+
+      <h3 className='flex justify-center text-3xl my-8 mb-5'>
+        {strings.companyOurAreas}
+      </h3>
+
+      <ul className='flex flex-col items-center gap-8'>
+        {useCreateSection(router)}
+      </ul>
     </main>
   );
 }
